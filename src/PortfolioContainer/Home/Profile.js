1f7814d30f8cbfd3de493e066391fca743038e08
Priyanka Sarda <piusarda0@gmail.com>
2023-03-03 21:07:54 +0530

    Some work of portfolio

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+      <div className='profile-parent'>
+        <div className='profile-details'>
+            <div className='colz'>
+                <a href='https://www.facebook.com/priyanka.sarda.5'>
+                    <i className='fa fa-facebook-square'></i>
+                </a>
+                <a href='#'>
+                    <i className='fa fa-google-plus-square'></i>
+                </a>
+                <a href='#'>
+                    <i className='fa fa-instagram'></i>
+                </a>
+                <a href='https://www.linkedin.com/in/priyanka-sarda-145aa924b/'>
+                    <i className='fa fa-linkedin'></i>
+                </a>
+                <a href='https://github.com/Piusarda'>
+                    <i className='fa fa-github'></i>
+                </a>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
